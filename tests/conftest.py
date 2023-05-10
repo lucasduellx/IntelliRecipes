3e46ae327350be889e3f3dfbb4fd9aa323c64e41
Joker <victorassagra@hotmail.com>
2023-05-09 21:34:14 -0300

    teste

---
+++ tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest, sqlite3
+from flask import session
+
+import sys
+import os
+
+# Add the root directory of your Flask application to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app
+
+@pytest.fixture()
+def test_app():
+    test_app = app
+    test_app.config.update({
+        "TESTING": True,
+        #"LOGIN_DISABLED": True,
+    })
+    
+    yield test_app
+
+
+@pytest.fixture()
+def client(test_app):
+    return test_app.test_client()
---
+++ tests/test_session.py
@@ -0,0 +1,18 @@
+from flask import session
+
+def test_access_session(client):
+    with client.session_transaction() as session:
+        session["user_id"] = 1
+        session["user_name"] = 'test_user'
+
+
+def test_redirect(client):
+    with client:
+        response = client.get("/")
+        # "/" redirects either to login or adventure
+        assert response.status_code == 302
+
+def test_access(client):
+    with client:
+        response = client.get("/login")
+        assert response.status_code == 200
